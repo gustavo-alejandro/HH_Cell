ee1e7ee3669aa4edd44e38c0cdd4bc7f6504f0c5
gustavo-alejandro <gaospinaa@unal.edu.co>
2023-07-28 18:40:31 +1000

    added anode fanning factors

--- HH_CellGUI.py
+++ HH_CellGUI.py
@@ -2,13 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 import math
 from PIL import Image, ImageTk
+from statistics import mean
 
 
 class Cell_input:
     def __init__(self):
         # Initialize attribute values
-        self.current = tk.IntVar(value=300)
-        self.ACD = tk.DoubleVar(value=3)
+        self.current = tk.IntVar(value=280)
+        self.ACD = tk.DoubleVar(value=3.45)
 
 class Bath:
     def __init__(self):
@@ -108,19 +109,59 @@ class Anode:
 
     def __init__(self):
         # Initialize attribute values
-        self.length = tk.IntVar(value=1600)
-        self.width = tk.IntVar(value=800)
-        self.height = tk.IntVar(value=600)
-        self.age = tk.IntVar(value=0.0)
+        self.length_new = tk.DoubleVar(value=1850)
+        self.length_spent = tk.DoubleVar(value=1850)
+        self.width_new = tk.DoubleVar(value=690)
+        self.width_spent = tk.DoubleVar(value=690)
+        self.height = tk.DoubleVar(value=655)
+        self.depth_immers = tk.DoubleVar(value=14.9)
+        self.age = tk.DoubleVar(value=0.0)
         self.n_anodes = tk.IntVar(value=36)
+        self.S_1 = tk.DoubleVar(value=25) # Distance anode-wall 1 (cm)
+        self.S_2 = tk.DoubleVar(value=6) # Distance between anode short sidewalls 2 (cm)
+        self.S_3 = tk.DoubleVar(value=12) # Distance between anode long sidewalls 3 (cm)
+        self.S_4 = tk.DoubleVar(value=6) # Distance anode-wall 4 (cm)
 
-    def bot_surface_area(self):
-        self.area = self.length.get()*.01 * self.width.get()*.01
+    def fanning_factor(self, ACD, S_i):
+        """
+        Parameter names from original equation:
+        Dac: Anode to cathode distance ACD [cm]
+        La: Average of new and spent anode length [cm]
+        Wa: Average of new and spent anode width [cm]
+        h: Anode immersion depth [cm]
+        Si: Distance to adjacent anode or twice the distance to an insulating wall [cm]
+        From paper titled 'Haupin, W. Interpreting the components of cell voltage'
+        Eq. 34
+        https://doi.org/10.1007/978-3-319-48156-2_21
+
+        :param ACD:
+        :return:
+        """
+        length_avg = mean([self.length_new.get(), self.length_spent.get()])*.1
+        width_avg = mean([self.width_new.get(), self.width_spent.get()])*.1
+        self.factor = (0.1656 * ACD - 0.0043 * pow(ACD, 3) + 0.1270 * S_i - 0.0034 * pow(S_i, 2) + 0.0394 * ACD * S_i) * (
+            0.3844 + 0.06166 * self.depth_immers.get() + 0.001822 * (length_avg + width_avg) - 0.000178 * self.depth_immers.get() * (length_avg + width_avg)
+        )
+        return self.factor
+    def bath_eff_area(self, ACD):
+        length_avg = mean([self.length_new.get(), self.length_spent.get()]) * .1
+        width_avg = mean([self.width_new.get(), self.width_spent.get()]) * .1
+        print(f"length: {length_avg}, width: {width_avg}")
+        S1 = self.S_1.get()
+        S2 = self.S_2.get()
+        S3 = self.S_3.get()
+        S4 = self.S_4.get()
+        F1 = self.fanning_factor(ACD, S1)
+        F2 = self.fanning_factor(ACD, S2)
+        F3 = self.fanning_factor(ACD, S3)
+        F4 = self.fanning_factor(ACD, S4)
+        print(f"Fanning factors are: {F1}, {F2}, {F3}, {F4}")
+        self.area = (length_avg + F1 + F2)  * (width_avg + F3 + F4)
         return self.area
 
-    def current_intensity(self, current):
-        bot_anode_surface = self.bot_surface_area()
-        current_intensity = current / bot_anode_surface / self.n_anodes.get()
+    def current_intensity(self, current, n_anodes, ACD):
+        bot_anode_surface = self.bath_eff_area(ACD)
+        current_intensity = (current*1000) / bot_anode_surface / n_anodes
         return current_intensity
 
 class CellGUI:
@@ -131,6 +172,7 @@ class CellGUI:
         # Initialize the bath object and anode object
         self.bath = Bath()
         self.anode = Anode()
+        self.cell = Cell_input()
 
         # Load and display the image
         self.image_path = "C:/Users/n11675250/OneDrive - Queensland University of Technology/Aluminium Cell/E/hhprg/cellschema/images/left_riser_gesamt1.png"
@@ -192,36 +234,83 @@ class CellGUI:
         anode_frame = ttk.LabelFrame(self.root, text="Anode data")
         anode_frame.grid(row=0, column=6, padx=5, pady=10, sticky="nw")
 
-        ttk.Label(anode_frame, text="Length", width=6).grid(row=0, column=0, padx=5, pady=5)
-        length_entry = ttk.Entry(anode_frame, textvariable=self.anode.length, width=5)
-        length_entry.grid(row=0, column=1, padx=1, pady=1)
+        ttk.Label(anode_frame, text="Length new", width=12).grid(row=0, column=0, padx=5, pady=5)
+        length_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.length_new, width=5)
+        length_new_entry.grid(row=0, column=1, padx=1, pady=1)
 
-        ttk.Label(anode_frame, text="Width", width=6).grid(row=1, column=0, padx=5, pady=5)
-        width_entry = ttk.Entry(anode_frame, textvariable=self.anode.width, width=5)
-        width_entry.grid(row=1, column=1, padx=5, pady=5)
+        ttk.Label(anode_frame, text="Length spent", width=12).grid(row=1, column=0, padx=5, pady=5)
+        length_old_entry = ttk.Entry(anode_frame, textvariable=self.anode.length_spent, width=5)
+        length_old_entry.grid(row=1, column=1, padx=1, pady=1)
 
-        ttk.Label(anode_frame, text="Height", width=6).grid(row=2, column=0, padx=5, pady=5)
+        ttk.Label(anode_frame, text="Width new", width=12).grid(row=2, column=0, padx=5, pady=5)
+        width_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.width_new, width=5)
+        width_new_entry.grid(row=2, column=1, padx=5, pady=5)
+
+        ttk.Label(anode_frame, text="Width spent", width=12).grid(row=3, column=0, padx=5, pady=5)
+        width_old_entry = ttk.Entry(anode_frame, textvariable=self.anode.width_spent, width=5)
+        width_old_entry.grid(row=3, column=1, padx=5, pady=5)
+
+        ttk.Label(anode_frame, text="Height", width=6).grid(row=4, column=0, padx=5, pady=5)
         height_entry = ttk.Entry(anode_frame, textvariable=self.anode.height, width=5)
-        height_entry.grid(row=2, column=1, padx=5, pady=5)
+        height_entry.grid(row=4, column=1, padx=5, pady=5)
 
-        ttk.Label(anode_frame, text="Age", width=6).grid(row=3, column=0, padx=5, pady=5)
+        ttk.Label(anode_frame, text="Age", width=6).grid(row=5, column=0, padx=5, pady=5)
         age_entry = ttk.Entry(anode_frame, textvariable=self.anode.age, width=5)
-        age_entry.grid(row=3, column=1, padx=5, pady=5)
+        age_entry.grid(row=5, column=1, padx=5, pady=5)
 
-        ttk.Label(anode_frame, text="n anodes", width=6).grid(row=4, column=0, padx=5, pady=5)
+        ttk.Label(anode_frame, text="n anodes", width=6).grid(row=6, column=0, padx=5, pady=5)
         n_anodes_entry = ttk.Entry(anode_frame, textvariable=self.anode.n_anodes, width=5)
-        n_anodes_entry.grid(row=4, column=1, padx=5, pady=5)
+        n_anodes_entry.grid(row=6, column=1, padx=5, pady=5)
+
+        ttk.Label(anode_frame, text="Depth subm", width=6).grid(row=7, column=0, padx=5, pady=5)
+        depth_imm_entry = ttk.Entry(anode_frame, textvariable=self.anode.depth_immers, width=5)
+        depth_imm_entry.grid(row=7, column=1, padx=5, pady=5)
+
+        # Anode spacing input GUI
+
+        anode_frame = ttk.LabelFrame(self.root, text="Anode spacing")
+        anode_frame.grid(row=0, column=7, padx=5, pady=10, sticky="nw")
+
+        ttk.Label(anode_frame, text="S_1", width=12).grid(row=0, column=0, padx=5, pady=5)
+        length_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.S_1, width=5)
+        length_new_entry.grid(row=0, column=1, padx=1, pady=1)
+
+        ttk.Label(anode_frame, text="S_2", width=12).grid(row=1, column=0, padx=5, pady=5)
+        length_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.S_2, width=5)
+        length_new_entry.grid(row=1, column=1, padx=1, pady=1)
+
+        ttk.Label(anode_frame, text="S_3", width=12).grid(row=2, column=0, padx=5, pady=5)
+        length_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.S_3, width=5)
+        length_new_entry.grid(row=2, column=1, padx=1, pady=1)
+
+        ttk.Label(anode_frame, text="S_4", width=12).grid(row=3, column=0, padx=5, pady=5)
+        length_new_entry = ttk.Entry(anode_frame, textvariable=self.anode.S_4, width=5)
+        length_new_entry.grid(row=3, column=1, padx=1, pady=1)
+
 
         #Anode calculations output frame GUI
 
         anode_calc_frame = ttk.LabelFrame(self.root, text="anode calc")
         anode_calc_frame.grid(row=1, column=6, padx=5, pady=5, sticky="nw")
 
-        self.bot_surface_field_gui = ttk.Label(anode_calc_frame, text="Bottom area:     cm2")
-        self.bot_surface_field_gui.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
+        self.bath_eff_area_field_gui = ttk.Label(anode_calc_frame, text="Bath eff area:     cm2")
+        self.bath_eff_area_field_gui.grid(row=0, column=0, padx=5, pady=5, sticky="nw")
 
         self.current_density_field_gui = ttk.Label(anode_calc_frame, text="Current density:    A/cm2 ")
         self.current_density_field_gui.grid(row=1, column=0, padx=5, pady=5, sticky="nw")
+
+        #Cell input frame GUI
+
+        cell_frame = ttk.LabelFrame(self.root, text="Cell input data")
+        cell_frame.grid(row=1, column=0, padx=5, pady=10, sticky="nw")
+
+        ttk.Label(cell_frame, text="Current", width=10).grid(row=0, column=0, padx=5, pady=5)
+        cell_current_entry = ttk.Entry(cell_frame, textvariable=self.cell.current, width=6)
+        cell_current_entry.grid(row=0, column=1, padx=1, pady=1)
+
+        ttk.Label(cell_frame, text="ACD", width=10).grid(row=1, column=0, padx=5, pady=5)
+        ACD_entry = ttk.Entry(cell_frame, textvariable=self.cell.ACD, width=6)
+        ACD_entry.grid(row=1, column=1, padx=1, pady=1)
 
         ttk.Button(self.root, text="Calculate", command=self.update_results).grid(row=5, column=0, columnspan=3, padx=10, pady=10)
 
@@ -262,28 +351,29 @@ class CellGUI:
 
         try:
             # Get the user input values for anode attributes
-            length = self.anode.length.get()
-            width = self.anode.width.get()
-            height = self.anode.height.get()
-            age = self.anode.age.get()
+            # length = self.anode.length_new.get()
+            # width = self.anode.width_new.get()
+            # height = self.anode.height.get()
+            # age = self.anode.age.get()
             n_anodes = self.anode.n_anodes.get()
-
-            # Update the anode object's attributes with the new values
-            self.anode.length.set(length)
-            self.anode.width.set(width)
-            self.anode.height.set(height)
-            self.anode.age.set(age)
+            #
+            # # Update the anode object's attributes with the new values
+            # self.anode.length_new.set(length)
+            # self.anode.width_new.set(width)
+            # self.anode.height.set(height)
+            # self.anode.age.set(age)
 
             # Calculate and display bot_anode_surface
-            bot_anode_surface = self.anode.bot_surface_area()
-            self.bot_surface_field_gui.config(text=f"Bottom area: {bot_anode_surface:.2f} cm2")
+            ACD = self.cell.ACD.get()
+            bot_anode_surface = self.anode.bath_eff_area(ACD)
+            self.bath_eff_area_field_gui.config(text=f"Bath eff area: {bot_anode_surface:.2f} cm2")
             # Calculate current_intensity using bot_anode_surface and a constant current
-            current = 1000  # You can replace this value with the desired current intensity
-            current_intensity = self.anode.current_intensity(current)
+            current = self.cell.current.get()  # You can replace this value with the desired current intensity
+            current_intensity = self.anode.current_intensity(current, n_anodes, ACD)
             self.current_density_field_gui.config(text=f"Current intensity: {current_intensity:.2f} A/cm2")
         except Exception as e:
             print(f"Error calculating anode attributes: {e}")
-            self.bot_anode_surface_label.config(text="error")
+            self.bath_eff_area_field_gui.config(text="error")
             #self.current_intensity_label.config(text="error")
 
 if __name__ == "__main__":
